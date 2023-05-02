f11730dbb46247bce5206aecb3b50cd6317a21f1
Shivam Pasvan <112854390+Shivampasvan@users.noreply.github.com>
2023-05-02 16:34:03 +0530

    Add files via upload

---
+++ Post.routes.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const postRouter = express.Router();
+const { PostModel } = require("../model/Post.model");
+
+postRouter.post("/create", async (req, res) => {
+  try {
+    const post = new PostModel(req.body);
+    await post.save();
+    res.status(200).send({ msg: "New post has been created" });
+  } catch (error) {
+    res.status(400).send({ err: error.message });
+  }
+});
+
+postRouter.get("/", async (req, res) => {
+  try {
+    const posts = await PostModel.find({ userID: req.body.userID });
+    res.status(200).send(posts);
+  } catch (error) {
+    res.status(400).send({ err: error.message });
+  }
+});
+
+postRouter.patch("/update/:postID", async (req, res) => {
+  const { postID } = req.params;
+  const post = await PostModel.findOne({ _id: postID });
+  try {
+    if (req.body.userID !== post.userID) {
+      res.status(200).send({ msg: `You are not authorized to do this action` });
+    } else {
+      await PostModel.findByIdAndUpdate({ _id: postID }, req.body);
+      res
+        .status(200)
+        .send({ msg: `The post with id: ${postID} has been updated.` });
+    }
+  } catch (error) {
+    res.status(400).send({ err: error.message });
+  }
+});
+
+postRouter.delete("/delete/:postID", async (req, res) => {
+  const { postID } = req.params;
+  const post = await PostModel.findOne({ _id: postID });
+  try {
+    if (req.body.userID !== post.userID) {
+      res.status(200).send({ msg: `You are not authorized to do this action` });
+    } else {
+      await PostModel.findByIdAndDelete({ _id: postID });
+      res
+        .status(200)
+        .send({ msg: `The post with id: ${postID} has been deleted.` });
+    }
+  } catch (error) {
+    res.status(400).send({ err: error.message });
+  }
+});
+
+module.exports = {
+  postRouter,
+};
